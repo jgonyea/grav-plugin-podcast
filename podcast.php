4a415353bad36488b67314d8a161de197d4a21a4
Jeremy Gonyea <jeremy.gonyea@gmail.com>
2017-07-04 01:20:46 -0400

    More work on fields for podcast entry and podcast channel. RSS feed from channel page follows iTunes example

--- podcast.php
+++ podcast.php
@@ -66,4 +66,15 @@ class PodcastPlugin extends Plugin
       $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
   }
   
+  /**
+   * Generate GUID for podcast entry.
+   */
+  public static function generateGuid($length = 20, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+    $str = '';
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+        $str .= $keyspace[random_int(0, $max)];
+    }
+    return $str;
+  }
 }
